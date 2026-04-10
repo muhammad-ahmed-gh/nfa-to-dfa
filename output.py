5b37f623ea08a49c4df08c5eb5a1d114ccf13d26
youssefnasr2006 <yossef.hassan.2022006@gmial.com>
2026-04-10 21:41:38 +0200

    output function

--- output.py
+++ output.py
@@ -12,32 +12,52 @@
 #   }
 # }
 #output
+# ---------DFA Table---------
 # Alphabet: ['0', '1']
 # States: ['A', 'B', 'C']
 # Start state: A
 # Accept state: ['B', 'C']
-# Transitions:
-# →A ——— 0 ——» →A
-# →A ——— 1 ——» C
+# Transitions Table:
+# ---------------------------
+# State    |input(0) |input(1) |
+# ---------------------------
+#  →A      | →A      | *C      |
+#...
 def displayResult(result_DFA):
     print(f"---------DFA Table---------")
     print(f"Alphabet: {result_DFA['alphabet']}")
     print(f"States: {result_DFA['states']}")
     print(f"Start state: {result_DFA['start']}")
     print(f"Accept state: {result_DFA['accepting']}")
-    print(f"Transitions:")
-    for state,transition in result_DFA['transitions'].items():
-        prefix=""
-        if(state == result_DFA['start']):
-            prefix+="→"
-        if(state == result_DFA['accepting']):
-            prefix+="*"
-        for symbol,Nextstate in transition.items():
-            prefixnext=""
-            if(Nextstate == result_DFA['start']):
-                prefixnext+="→"
-            if(Nextstate == result_DFA['accepting']):
-                prefixnext+="*"
+    print(f"Transitions Table:")
+    print("-------"+"----------"*len(result_DFA['alphabet']))
+    print("State",end="    |")
+    for alpha in result_DFA['alphabet']:
+        print(f"input({alpha})", end=" |")
+    print("\n-------"+"----------"*len(result_DFA['alphabet']))
+    for state in result_DFA['states']:
+        prefix="  "
+        if(state in result_DFA['accepting']) and (state in result_DFA['start']):
+            prefix="→*"
+        elif(state in result_DFA['accepting']):
+            prefix=" *"
+        elif(state in result_DFA['start']):
+            prefix=" →"       
+        print(f"{prefix}{state:<4}", end="")
+        if state in result_DFA['transitions']:
+            transition = result_DFA['transitions'][state]
+            print(end="   |")
+            for symbol in result_DFA['alphabet']:
+                if symbol in transition:
+                    Nextstate = transition[symbol]                    
+                    prefixnext="  "
+                    if(Nextstate in result_DFA['start']) and (Nextstate in result_DFA['accepting']):
+                        prefixnext="→*"
 
-            print(f"{prefix}{state} ——— {symbol} ——» {prefixnext}{Nextstate}")
-        print("\n")
+                    elif(Nextstate in result_DFA['accepting']):
+                        prefixnext=" *"
+                    elif(Nextstate in result_DFA['start']):
+                        prefixnext=" →"
+
+                    print(f"{prefixnext}{Nextstate:<7}", end="|")
+        print(end="\n")
