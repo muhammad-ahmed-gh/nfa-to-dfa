dbf776347f5209651ee95034f260925f418b5321
muhammad-ahmed-gh <muhammad.ahmed.connect@gmail.com>
2026-04-10 23:32:08 +0200

    add input function

--- input.py
+++ input.py
@@ -1,2 +1,23 @@
 def getNFA():
-  pass
+    NFA = {}
+    print("Enter the following separated with commas\n")
+
+    NFA['states'] = input("States of the NFA: ").split(",")
+    NFA['alphabet'] = input("Alphabet of the NFA: ").split(",")
+    NFA['start'] = input("Start state of the NFA: ")
+    NFA['accepting'] = input("Accepting states of the NFA: ").split(",")
+    NFA['transitions'] = {}
+
+    for state in NFA['states']:
+        NFA['transitions'][state] = {}
+        for symbol in NFA['alphabet']:
+            next_states = input(f"{state} at {symbol} -> ").split(",")
+            NFA['transitions'][state][symbol] = next_states
+
+        eps = input(f"{state} at ε -> ")
+        if eps.strip() == "":
+            NFA['transitions'][state]["ε"] = []
+        else:
+            NFA['transitions'][state]["ε"] = eps.split(",")
+
+    return NFA
--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from validation import validate
 from processing import DFA
 from output import displayResult
 
+print("-----------------------------------------")
+print("        -- NFA to DFA Converter --")
+print("-----------------------------------------")
+
 NFA = getNFA()
 isValid, errMsg = validate(NFA)
 
